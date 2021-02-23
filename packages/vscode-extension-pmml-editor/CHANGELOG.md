86a85a386203638d04df1e3837fe2fd3d1c2be0f
Michael Anstis <manstis@redhat.com>
2021-02-23 10:54:36 +0000

    FAI-419: Score Cards: Write README for VSCode Marketplace

---
+++ packages/vscode-extension-pmml-editor/CHANGELOG.md
@@ -0,0 +1,6 @@
+# 0.8.3 (alpha)
+
+## New features:
+
+Editors
+*   [[FAI-174](https://issues.redhat.com/browse/FAI-174)] - [Scorecard Editor] Initial pre-release
---
+++ packages/vscode-extension-pmml-editor/webpack.config.js
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const path = require("path");
+const pfWebpackOptions = require("@kogito-tooling/patternfly-base/patternflyWebpackOptions");
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+
+const commonConfig = {
+    mode: "development",
+    devtool: "inline-source-map",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "[name].js",
+        library: "PmmlEditor",
+        libraryTarget: "umd",
+        umdNamedDefine: true
+    },
+    externals: {
+        vscode: "commonjs vscode"
+    },
+    plugins: [],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            },
+            ...pfWebpackOptions.patternflyRules
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js", ".jsx"],
+        modules: [path.resolve("../../node_modules"), path.resolve("./node_modules"), path.resolve("./src")]
+    }
+};
+
+module.exports = async (argv) => [
+    {
+        ...commonConfig,
+        target: "node",
+        entry: {
+            "extension/extension": "./src/extension/extension.ts"
+        },
+        plugins: []
+    },
+    {
+        ...commonConfig,
+        target: "web",
+        entry: {
+            "webview/index": "./src/webview/index.ts"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ttf$/,
+                    use: ["file-loader"]
+                },
+                ...pfWebpackOptions.patternflyRules
+            ]
+        },
+        plugins: [new MonacoWebpackPlugin()]
+    }
+];
